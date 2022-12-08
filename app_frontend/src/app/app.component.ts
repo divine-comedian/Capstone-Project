d06ab0d2ac9ef3021e06f10e60e0d32ad82a6566
Arian Hojat <armyofda12mnkeys@gmail.com>
2022-12-08 06:02:10 +0000

    add a simple Connect button for now

--- app_frontend/src/app/app.component.ts
+++ app_frontend/src/app/app.component.ts
@@ -1,6 +1,19 @@
 
 import { Component } from '@angular/core';
+import { BigNumber, ethers } from 'ethers';
 import { SalesContractService } from "./services/sales-contract.service";
+
+import salesContractInterface  from '../assets/SalesContractHere.json'; //Lottery/Auction
+import salesFactoryContractInterface  from '../assets/SalesFactoryContractHere.json'; //Factory
+
+const SALES_FACTORY_ADDRESS = "0x"; //move to a Service maybe?
+const SALES_ADDRESS = "0x"; //move to a Service maybe?
+
+declare global {
+  interface Window {
+    ethereum: any
+  }
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,9 +22,44 @@ import { SalesContractService } from "./services/sales-contract.service";
 export class AppComponent {
   title = 'Test Routing';
   //someRootVar = 'SharedRootVar';
+  provider: ethers.providers.BaseProvider | undefined;
+  signer: ethers.Signer | undefined;
+  walletAddress: string | undefined;
+  salesFactoryContract: ethers.Contract | undefined;
+  salesContract: ethers.Contract | undefined;
+  lastNetworkBlock: number | undefined; //remove this later, just testing connecting to Goerli
+
   constructor(
     private salesContractService: SalesContractService
   ) {}
+
+  
+  
+  async connectWallet(){
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    // Prompt user for account connections
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();    
+    this.signer = signer;
+    this.walletAddress = await this.signer.getAddress();
+    console.log("walletAddress is: "+ this.walletAddress);
+    const connectedAccountAddr = await signer.getAddress();
+    console.log("Account:", connectedAccountAddr);    
+    
+    const lastBlock = await provider.getBlock("latest"); //remove this later, just testing connecting to Goerli
+    console.log('last block on network is: '+ lastBlock.number);
+    this.lastNetworkBlock = lastBlock.number; 
+
+    this.provider = provider; //ethers.getDefaultProvider("goerli", {alchemy: ALCHEMY_API_KEY, etherscan: ETHERSCAN_API_KEY});
+    //this.salesFactoryContractAddress = SALES_FACTORY_ADDRESS;
+    //this.salesContractAddress = SALES_ADDRESS;
+    //this.salesFactoryContract = new ethers.Contract( SALES_FACTORY_ADDRESS, salesFactoryContractInterface.abi, signer);
+    //this.salesContract = new ethers.Contract( SALES_ADDRESS , salesFactoryContractInterface.abi, signer); //could be hardcoded vs returning from API
+
+    //this.updateBlockchainInfo();
+    //setInterval( this.updateBlockchainInfo.bind(this), 5000);
+  }
+
 }
 
 
