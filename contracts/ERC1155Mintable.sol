9560516c6a67a37f302b31fac1c8aff5021c69ac
onizuka- <findthefunction@gmail.com>
2022-12-12 03:34:00 +0000

    added functionality https://discord.com/channels/705799923014041651/1049476627521871934/1050774898903109645

--- contracts/ERC1155Mintable.sol
+++ contracts/ERC1155Mintable.sol
@@ -2,44 +2,48 @@
 pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts@4.8.0/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts@4.8.0/access/Ownable.sol";
 import "@openzeppelin/contracts@4.8.0/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts@4.8.0/access/AccessControl.sol";
+import "@openzeppelin/contracts@4.8.0/token/ERC20/ERC20.sol";
 
-contract PixelsForPeace is ERC1155, Ownable, ERC1155Supply {
+contract ERC1155Mintable is ERC1155, AccessControl {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
     mapping(uint256 => string) private _tokenURIs;
+    
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-    constructor() ERC1155("") {
+    constructor(address minter) ERC1155("PixelsForPeace") {
+        _grantRole(MINTER_ROLE, minter);
     }
 
-     function _setTokenURI(uint256 tokenId, string memory _tokenURI)
-     internal
-     virtual
-     {
-        _tokenURIs[tokenId] = _tokenURI;
-     }
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC1155, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _setTokenURI(uint256 tokenId, string memory _tokenURI)
+    internal
+    virtual
+    {
+       _tokenURIs[tokenId] = _tokenURI;
+    }
 
     function mint(address recipient, string memory _tokenURI)
         public
-        onlyOwner
+    
         returns (uint256)
+        // Check that the calling account has the minter role
     {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+
        _tokenIds.increment();
        uint256 newItemId = _tokenIds.current();
        _mint(recipient, newItemId, 1, "");
        _setTokenURI(newItemId, _tokenURI);
        return newItemId;
+
     }
 
-    // The following functions are overrides required by Solidity.
-
-    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        internal
-        override(ERC1155, ERC1155Supply)
-    {
-        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
-    }
 
 }
