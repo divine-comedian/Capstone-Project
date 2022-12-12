e6c44ef8deabbd799d69ab88169d808f6da7ace3
Arian Hojat <armyofda12mnkeys@gmail.com>
2022-12-12 06:33:10 +0000

    show TokenBalance and start bet

--- app_frontend/src/app/components/lottery/lottery.component.ts
+++ app_frontend/src/app/components/lottery/lottery.component.ts
@@ -5,7 +5,8 @@ import { Location } from '@angular/common';
 import { SalesContractService } from 'src/app/services/sales-contract.service';
 
 import saleLotteryInterface  from '../../../assets/Lottery.json';
-import { Contract } from 'ethers';
+import lotteryTokenInterface  from '../../../assets/LotteryToken.json';
+import { BigNumber, Contract, ethers } from 'ethers';
 
 @Component({
   selector: 'app-lottery',
@@ -20,6 +21,9 @@ export class LotteryComponent implements OnInit {
   closingTime: number | undefined; 
   closingTimeDateLocalized: Date | undefined;
   ownerPool: number | undefined;
+  paymentToken: string | undefined;
+  tokenBalance: number | undefined;
+  tokenBalanceSmallerUnits:  number | undefined;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -51,6 +55,20 @@ export class LotteryComponent implements OnInit {
           console.log('On Lottery component page, ownerPool is:'+ x );
           this.ownerPool = x;
         });
+        this.salesContractService.getLotteryInfoPaymentToken(this.contract_addr, saleLotteryInterface, true).then((x:string)=>{
+          console.log('On Lottery component page, ownerPool is:'+ x );
+          this.paymentToken = x;
+          if(this.paymentToken) {
+            this.salesContractService.getLotteryTokenBalance(this.paymentToken, lotteryTokenInterface, true).then((x:number)=>{
+              console.log('On Lottery component page, get lottery token balance:'+ x );
+              //console.log( tokenBalanceBigNumber );
+              //console.log( ethers.utils.formatEther(tokenBalanceBigNumber) );
+              this.tokenBalance = parseFloat( ethers.utils.formatUnits(x, 18) ); //TODO: double-check units for this token are 18
+              this.tokenBalanceSmallerUnits = parseFloat( x.toString() );
+            });
+          }
+        });
+        
         
 
         
@@ -68,6 +86,17 @@ export class LotteryComponent implements OnInit {
     const contract_addr = this.activatedRoute.snapshot.paramMap.get('contract_addr');
 
     this.contract_addr = contract_addr;
+  }
+
+  bet() {
+    console.log('lottery.bet()');
+    if(this.contract_addr && this.paymentToken) {
+      this.salesContractService.postLotteryBet(this.contract_addr, saleLotteryInterface, this.paymentToken, lotteryTokenInterface, true).then((x:string)=>{
+        console.log('bet transaction done:'+ x);
+        alert('You have betted successfully!');
+      });
+    }
+
   }
 
 }
